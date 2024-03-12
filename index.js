e5a2829e9687bb9e7570cef0d309a8ee40a81728
Sunny <bobby149@gmail.com>
2024-03-12 13:57:39 +0530

    some changes

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log("done");
 
+let navbar=document.getElementById("scnav");
+
+// console.log(data);
+window.addEventListener("scroll",function(){
+    let Yscrool=Math.round(window.scrollY);
+    if(Yscrool>153&window.innerWidth>991){
+        navbar.classList.add("fixed-top");
+    }
+    else{
+        navbar.classList.remove("fixed-top");
+ 
+    }
+})
