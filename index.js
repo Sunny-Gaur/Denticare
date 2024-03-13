a8bcf4095333aef08e3ed6e3d1b35d1cec73d6e7
Sunny <bobby149@gmail.com>
2024-03-13 10:18:01 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 console.log("done");
 
 let navbar=document.getElementById("scnav");
+let Triangle=document.querySelector(".triangle");
+
 
 // console.log(data);
 window.addEventListener("scroll",function(){
     let Yscrool=Math.round(window.scrollY);
     if(Yscrool>153&window.innerWidth>991){
         navbar.classList.add("fixed-top");
+        navbar.classList.add("fixed-top-data");
+
     }
     else{
         navbar.classList.remove("fixed-top");
- 
+        navbar.classList.remove("fixed-top-data");
+
     }
+    
+    if(Yscrool>772){
+        Triangle.style.display="block";
+    }
+    else{
+        Triangle.style.display="none";
+    }
+   
+})
+let btn=document.querySelector("#up-down-btn");
+btn.addEventListener("click",()=>{
+    document.documentElement.scrollTop=0;
 })
